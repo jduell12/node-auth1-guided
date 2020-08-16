e209bb9364abdb76e9b9cc3ad87da73480d71c18
Jessica <duellje@oregonstate.edu>
2020-08-16 14:14:58 -0400

    added session config

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
 const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "monkey",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false, //ok in development but want true in production
+    httpOnly: true, //can't be accessed from JS
+  },
+  resave: false, //recreate session even if nothing has changed?
+  saveUninitialized: false, //GDPR compliance - laws against setting cookies automatically
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 
