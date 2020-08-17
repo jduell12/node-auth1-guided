3bdf4908c67143deb836809d84a42eea55bcd9ad
Jessica <duellje@oregonstate.edu>
2020-08-17 13:04:30 -0400

    added bcrypt

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
 const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router");
 
 const server = express();
 
@@ -11,9 +13,23 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
+});
+
+server.get("/hash", (req, res) => {
+  //read a password property from the headers
+  //hash the password and send back both the password and the hash
+  const password = req.headers.password;
+  try {
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcrypt.hashSync(password, rounds);
+    res.status(200).json({ password, hash });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = server;
---
+++ auth/auth-router.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  //validate user credentials, check password length, libraries for this
+
+  const rounds = process.env.HASH_ROUNDS || 8;
+  const hash = bcrypt.hashSync(password, rounds);
+
+  Users.add({ username, password: hash })
+    .then((users) => {
+      res.status(201).json({ data: users });
+    })
+    .catch((err) => {
+      res.json({ error: err.message });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user) {
+      } else {
+        res.status(401).json({ message: "Could not find username" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+module.exports = router;
